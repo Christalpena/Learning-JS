2713719fe96979c13cd36458416dbb7981ec1622
Christalpena <christalperez0@gmail.com>
2024-07-04 12:38:25 -0400

    Drawing figures

---
+++ square_and_triangle.js
@@ -0,0 +1,34 @@
+/*
+ * Crea un programa que dibuje un cuadrado o un triángulo con asteriscos "*".
+ * - Indicaremos el tamaño del lado y si la figura a dibujar es una u otra.
+ * - EXTRA: ¿Eres capaz de dibujar más figuras?
+ */
+
+
+function figures(size,fig){
+    
+    if(fig == 'Triangle'){
+        for(let i = 1; i <= size; i++){
+            let simbol = '* '
+            console.log(" ".repeat(size-i) + simbol.repeat(i))
+        }
+
+    }else if(fig == 'Rectangle'){
+        for(let i = 0; i < size; i++){
+            let simbol = '*  '
+            console.log(simbol.repeat(size*2))
+        }
+
+    }else{
+        for(let i = 0; i < size; i++){
+            let simbol = '*  '
+            console.log(simbol.repeat(size))
+        }
+    }
+
+    return `\n----${fig}----`
+}
+
+console.log(figures(6,'Rectangle'))
+console.log(figures(4,'Triangle'))
+console.log(figures(4,'Square'))
